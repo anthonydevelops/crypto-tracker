33c4cab2d7144f2b58606d609844deb5fdc8ab91
anthonydevelops <anthonydevelops@gmail.com>
2018-08-29 16:32:00 -0700

    Couldn't get async/await to work, promise 'pending', will revisit

--- client/src/components/currency.js
+++ client/src/components/currency.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Table, Media } from "reactstrap";
+import "../App.css";
 
 class Currency extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
-      data: {}
+      data: []
     };
   }
 
@@ -16,72 +15,56 @@ class Currency extends Component {
       "https://min-api.cryptocompare.com/data/top/totalvol?limit=20&tsym=USD"
     )
       .then(res => res.json())
-      .then(
-        result => {
-          console.log(result.Data);
-          this.setState({
-            isLoaded: true,
-            data: result.Data
-          });
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
+      .then(result => {
+        this.setState({
+          data: result.Data
+        });
+      });
   }
 
   render() {
-    const { error, isLoaded, data } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      // Return the data, which is held as {[ {}, {}, ... ]}
-      return (
-        <Table hover>
-          <thead>
-            <tr>
-              <th>Coin Name (Code)</th>
-              <th>Market Cap (USD)</th>
-              <th>Price (USD)</th>
-              <th>24h Change (%)</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map(item => {
-              return (
-                <tr>
-                  <th>
-                    <Media>
-                      <Media left href="#">
-                        <Media
-                          object
-                          sm
-                          src={
-                            "https://www.cryptocompare.com" +
-                            item.CoinInfo.ImageUrl
-                          }
-                          alt={item.CoinInfo.FullName}
-                        />
-                      </Media>
-                      <Media body>
-                        <Media heading>
-                          {item.CoinInfo.FullName} ({item.CoinInfo.Name})
-                        </Media>
+    const { data } = this.state;
+    // Return the data, which is held as {[ {}, {}, ... ]}
+    return (
+      <Table hover>
+        <thead>
+          <tr>
+            <th>Coin Name (Code)</th>
+            <th>Market Cap (USD)</th>
+            <th>Price (USD)</th>
+            <th>24h Change (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => {
+            return (
+              <tr>
+                <th>
+                  <Media>
+                    <Media href="#">
+                      <Media
+                        object
+                        className="cur-img"
+                        src={
+                          "https://www.cryptocompare.com" +
+                          item.CoinInfo.ImageUrl
+                        }
+                        alt={item.CoinInfo.FullName}
+                      />
+                    </Media>
+                    <Media body>
+                      <Media heading className="cur-header">
+                        {item.CoinInfo.FullName} ({item.CoinInfo.Name})
                       </Media>
                     </Media>
-                  </th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      );
-    }
+                  </Media>
+                </th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    );
   }
 }
 
